daa0d640af53e56686780fe72b72e5e9ab2bd85c
Nathan Osman <nathan@quickmediasolutions.com>
2023-05-20 09:34:08 -0700

    Add tests for Handler.

---
+++ handler_test.go
@@ -0,0 +1,97 @@
+package sse
+
+import (
+	"errors"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func runOnce(fn func()) func() {
+	hasRun := false
+	return func() {
+		if !hasRun {
+			fn()
+			hasRun = true
+		}
+	}
+}
+
+func TestHandler(t *testing.T) {
+	for _, v := range []struct {
+		Name              string
+		ChannelBufferSize int
+		Fn                func(
+			h *Handler,
+			hClose func(),
+			c *Client,
+			cClose func(),
+		) error
+	}{
+		{
+			Name: "basic event propagation",
+			Fn: func(h *Handler, hClose func(), c *Client, cClose func()) error {
+				h.Send(&Event{ID: "1"})
+				if err := receiveAtLeastNEvents(1, c, CLIENT_DELAY); err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+		{
+			Name: "handle client disconnect",
+			Fn: func(h *Handler, hClose func(), c *Client, cClose func()) error {
+				cClose()
+				time.Sleep(CLIENT_DELAY)
+				return func() error {
+					defer h.mutex.Unlock()
+					h.mutex.Lock()
+					if len(h.eventChans) != 0 {
+						return errors.New("client event channel still present")
+					}
+					return nil
+				}()
+			},
+		},
+		{
+			Name: "handle server disconnect",
+			Fn: func(h *Handler, hClose func(), c *Client, cClose func()) error {
+				hClose()
+				return nil
+			},
+		},
+	} {
+		func() {
+
+			// Create the handler & server
+			var (
+				h = NewHandler(&HandlerConfig{
+					ChannelBufferSize: v.ChannelBufferSize,
+				})
+				s      = httptest.NewServer(h)
+				hClose = runOnce(func() {
+					s.Listener.Close()
+					h.Close()
+					s.Close()
+				})
+			)
+			defer hClose()
+
+			// Create the client
+			c, err := NewClientFromURL(s.URL)
+			if err != nil {
+				t.Fatalf("%s: %s", v.Name, err)
+			}
+			cClose := runOnce(func() { c.Close() })
+			defer cClose()
+
+			// Wait for the client to connect
+			time.Sleep(CLIENT_DELAY)
+
+			// Run the test
+			if err := v.Fn(h, hClose, c, cClose); err != nil {
+				t.Fatalf("%s: %s", v.Name, err)
+			}
+		}()
+	}
+}
